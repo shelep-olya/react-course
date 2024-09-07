d4133a913fb0c967e8cdb8d1de0a2e62a0ed587c
shelep-olya <142796512+shelep-olya@users.noreply.github.com>
2024-09-07 09:32:25 +0300

    finished 6th section

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,5 +1,17 @@
+import { useState } from "react";
 import styles from "./ProductCard.module.css";
 export function ProductCard({ product, background = "slategray", onPurchase }) {
+  const [stockCount, setStockCount] = useState(product.stockCount);
+  const [showMore, setShowMore] = useState(false);
+  function handleClick() {
+    setStockCount((prevStockCount) => prevStockCount - 1);
+    onPurchase(product);
+  }
+
+  function handleTwoClicks() {
+    setStockCount((prevStockCount) => prevStockCount - 2);
+  }
+
   return (
     <article style={{ background }} className={styles.Container}>
       <h2>{product.title}</h2>
@@ -9,18 +21,27 @@ export function ProductCard({ product, background = "slategray", onPurchase }) {
         width={128}
         height={128}
       />
-      <p>Specification:</p>
-      <ul className={styles.Specification}>
-        {product.specification.map((spec, index) => (
-          <li key={index}>{spec}</li>
-        ))}
-      </ul>
-      <Status stockCount={product.stockCount} />
-      {product.stockCount > 0 && (
-        <button onClick={() => onPurchase(product)}>
-          Buy (From ${product.price})
+      <p>
+        Specification:{"   "}
+        <button onClick={() => setShowMore(!showMore)}>
+          {showMore ? "hide" : "show"}
         </button>
+      </p>
+      {showMore && (
+        <ul className={styles.Specification}>
+          {product.specification.map((spec, index) => (
+            <li key={index}>{spec}</li>
+          ))}
+        </ul>
       )}
+      <Status stockCount={stockCount} />
+      {stockCount > 0 && (
+        <>
+          <p>Price: ${product.price}</p>
+          <button onClick={handleClick}>Buy</button>
+        </>
+      )}
+      {stockCount > 1 && <button onClick={handleTwoClicks}>Buy 2</button>}
     </article>
   );
 }
